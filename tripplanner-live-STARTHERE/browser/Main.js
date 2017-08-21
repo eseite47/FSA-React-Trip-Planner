4644b72f678b3adb2b3f70bbbf89e8617985fe9f
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-08-20 20:24:07 -0400

    Complete Section B of react tutorial

--- tripplanner-live-STARTHERE/browser/Main.js
+++ tripplanner-live-STARTHERE/browser/Main.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react'
 import Map from './Map.js'
 import Navbar from './Navbar.js'
 import Panel from './Panel.js'
+import attractions from './attractions.js'
 
 export default class extends Component {
     constructor() {
@@ -13,14 +14,42 @@ export default class extends Component {
           restaurants: [],
           activities: []
         }
+        //this.addFunc = this.addFunc.bind(this)
     }
+
+    addFunc(attractionType) {
+      // get the selectedId from the DOM element corresponding to attractionType
+    const selectedId = document.getElementById(`${attractionType}-choices`).value
+      // retrieve the corresponding selectedAttraction data from `attractions.find()`
+    const selectedAttraction = attractions.find(attractionType, selectedId);
+      // clone this.state into a new object so that you can modify it
+    const newState = Object.assign({}, this.state)
+      // modify your cloned object with selectedAttraction
+      newState[attractionType].push(selectedAttraction)
+      // setState with the modified cloned object
+      this.setState(newState)
+    }
+
+    deleteFunc(attractionType, id) {
+      // clone this.state into a new object so that you can modify it
+      let newState = Object.assign({}, this.state)
+      // filter out anything that has the target id from the new object[attractionType]
+      newState[attractionType] = newState[attractionType].filter(place => place.id != id)
+      // setState with the modified cloned object
+      this.setState(newState)
+    }
+
     render() {
         return (<div>
                  <Navbar />
                 <div id="app" className="clearfix">
                   <Map />
-                  <Panel />
-                   </div>
+                  <Panel
+                    mainstate={this.state}
+                    addFunc={this.addFunc.bind(this)}
+                    deleteFunc={this.deleteFunc.bind(this)}
+                    />
+                </div>
                 </div>)
     }
 }
--- tripplanner-live-STARTHERE/browser/Panel.js
+++ tripplanner-live-STARTHERE/browser/Panel.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react'
+import attractions from './attractions.js'
 
 export default class extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
+        this.state = {
+          hotels: [],
+          restaurants: [],
+          activities: []
+        }
+        attractions.load()
+          .then(data => this.setState(data))
     }
     render() {
+        let makeOption = type => this.state[type].map((place, i) => <option value={place.id} key={i}>{place.name}</option>)
+        let makeLi = type => this.props.mainstate[type].map(place =>
+        <li className='itinerary-item'>
+          {place.name}
+          <button className ="btn btn-xs btn-danger remove btn-circle" onClick={()=>this.props.deleteFunc(type, place.id)}>x</button>
+        </li>)
         return (
           <div id="control-panel" className="col-sm-4 col-lg-3 clearfix">
             <div className="col-xs-6 col-sm-12">
@@ -13,20 +27,23 @@ export default class extends Component {
                   <div>
                     <h4>Hotels</h4>
                     <select id="hotels-choices">
+                      {makeOption('hotels')}
                   </select>
-                    <button id="hotels-add" className="btn btn-primary btn-circle pull-right">+</button>
+                    <button id="hotels-add" className="btn btn-primary btn-circle pull-right" onClick={()=>this.props.addFunc('hotels')}>+</button>
                   </div>
                   <div>
                     <h4>Restaurants</h4>
                     <select id="restaurants-choices">
+                     {makeOption('restaurants')}
                   </select>
-                    <button id="restaurants-add" className="btn btn-primary btn-circle pull-right">+</button>
+                    <button id="restaurants-add" className="btn btn-primary btn-circle pull-right" onClick={()=>this.props.addFunc('restaurants')}>+</button>
                   </div>
                   <div>
                     <h4>Activities</h4>
                     <select id="activities-choices">
+                     {makeOption('activities')}
                   </select>
-                    <button id="activities-add" className="btn btn-primary btn-circle pull-right">+</button>
+                    <button id="activities-add" className="btn btn-primary btn-circle pull-right" onClick={()=>this.props.addFunc('activities')}>+</button>
                   </div>
                 </div>
               </div>
@@ -39,18 +56,19 @@ export default class extends Component {
                   <div>
                   <h4>My Hotel</h4>
                   <ul className="list-group" id="hotels-list">
+                    {makeLi('hotels')}
                   </ul>
                   </div>
                   <div>
                   <h4>My Restaurants</h4>
                   <ul className="list-group" id="restaurants-list">
-
+                  {makeLi('restaurants')}
                   </ul>
                   </div>
                   <div>
                   <h4>My Activities</h4>
                   <ul className="list-group" id="activities-list">
-
+                  {makeLi('activities')}
                   </ul>
                   </div>
                 </div>
